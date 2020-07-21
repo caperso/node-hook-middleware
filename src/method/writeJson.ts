e8112ce8e5843d2a3000153b8a23b79a04c51140
caperso <273964183@qq.com>
2020-07-21 11:44:52 +0800

    update

--- src/method/writeJson.ts
+++ src/method/writeJson.ts
@@ -1,13 +1,25 @@
-import { writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 export function writeJsonPromise(
   jsonPath: string,
-  data: JSON
+  data: JSON,
+  propName?: string
 ): Promise<boolean | Error> {
   return new Promise((resolve, reject) => {
     try {
       const jsonString = JSON.stringify(data);
-      writeFileSync(jsonPath, jsonString);
+      let updatedJsonString = "";
+      if (propName) {
+        const content = JSON.parse(
+          readFileSync(jsonPath, { encoding: "utf-8" })
+        );
+        content[propName] = jsonString;
+        updatedJsonString = JSON.stringify(content);
+      } else {
+        updatedJsonString = jsonString;
+      }
+      writeFileSync(jsonPath, updatedJsonString);
+
       return resolve(true);
     } catch (e) {
       console.error(e);
--- src/middleware/useJsonStoreMiddleware.tsx
+++ src/middleware/useJsonStoreMiddleware.tsx
@@ -1,5 +1,22 @@
 import { useEffect } from "react";
+import { writeJsonPromise } from "../method/writeJson";
 
-const useStoreMiddleware = () => {
-  useEffect(() => {});
-};
+interface StoreProps<T> {
+  prisoner: T;
+  jsonPath: string;
+  jsonData: JSON;
+  jsonPropName?: string; // if propName is contained find it in
+}
+
+export default function useJsonStoreMiddleware<T>(props: StoreProps<T>): void {
+  const { prisoner, jsonPath, jsonData } = props;
+  useEffect(() => {
+    writeJsonPromise(jsonPath, jsonData)
+      .then((res) => {
+        console.log("write to local is completed");
+      })
+      .catch((e) => {
+        console.log("write to local is failed");
+      });
+  }, [prisoner]);
+}
